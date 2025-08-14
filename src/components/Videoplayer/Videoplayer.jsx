796c3f81852b238f7ebfb765acf385875c1000ca
Makeshsivam <makesh.p.2005@gmail.com>
2025-08-15 00:16:53 +0530

    may be last

--- src/components/Videoplayer/Videoplayer.jsx
+++ src/components/Videoplayer/Videoplayer.jsx
@@ -4,14 +4,19 @@ import Video1 from '../../assets/images/CAREVIDEO.mp4'
 
 const Videoplayer = ({playstae, setPlaystate}) => {
     const player= useRef(null);
+    const videoRef = useRef(null);
     const closeplayer = (e) => {
         if (e.target === player.current) {
+           if (videoRef.current) {
+              videoRef.current.pause();
+      }
+  
             setPlaystate(false);
         }
     }
   return (
     <div className={`videoplayer ${playstae ? '' : 'hide'}`} ref={player} onClick={closeplayer}>
-      <video src={Video1} autoPlay muted controls></video>
+      <video src={Video1} ref={videoRef} autoPlay muted controls ></video>
     </div>
   )
 }
